395171e25158b7d7638fcc4de6ceaa8e33997136
ahmadhanis <slumberjer@gmail.com>
2020-02-24 13:27:28 +0800

    24/2

--- grocery/lib/splashpage.dart
+++ grocery/lib/splashpage.dart
@@ -13,11 +13,60 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-              Image.asset("assets/images/grocery.png",scale: 2,)
+              Image.asset("assets/images/grocery.png",scale: 2,),
+              SizedBox(height: 10,),
+              ProgressIndicator()
             ],),
           ),
         ),
       ),
     );
+  }
+}
+
+class ProgressIndicator extends StatefulWidget {
+  @override
+  _ProgressIndicatorState createState() => new _ProgressIndicatorState();
+}
+
+class _ProgressIndicatorState extends State<ProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = Tween(begin: 0.0, end: 1.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          if (animation.value > 0.99) {
+           
+          }
+        });
+      });
+    controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    controller.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new Container(
+      width: 200,
+      //color: Colors.redAccent,
+      child: LinearProgressIndicator(
+        value: animation.value,
+        backgroundColor: Colors.black,
+        valueColor: new AlwaysStoppedAnimation<Color>(Colors.purple),
+      ),
+    ));
   }
 }
