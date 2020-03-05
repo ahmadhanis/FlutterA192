3826d5f71afaf5f166f262349e821b9ef48ce147
ahmadhanis <slumberjer@gmail.com>
2020-03-05 19:22:58 +0800

    5/3

---
+++ grocery/grocery/php/login_user.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(0);
+include_once("dbconnect.php");
+$email = $_POST['email'];
+$password = sha1($_POST['password']);
+
+$sql = "SELECT * FROM USER WHERE EMAIL = '$email' AND PASSWORD = '$password'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result ->fetch_assoc()){
+        echo "success";
+    }
+}else{
+    echo "failed";
+}
--- grocery/grocery/php/register_user.php
+++ grocery/grocery/php/register_user.php
@@ -6,12 +6,29 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $password = sha1($_POST['password']);
 
-    $sqlinsert = "INSERT INTO USER(NAME,EMAIL,PASSWORD,PHONE) VALUES ('$name','$email','$password','$phone')";
-    if ($conn->query($sqlinsert) === TRUE) {
-        echo "success";
-    } else {
-        echo "failed";
-    }
+$sqlinsert = "INSERT INTO USER(NAME,EMAIL,PASSWORD,PHONE) VALUES ('$name','$email','$password','$phone')";
+
+if ($conn->query($sqlinsert) === true)
+{
+    sendEmail($email);
+    echo "success";
     
+}
+else
+{
+    echo "failed";
+}
+
 //http://slumberjer.com/grocery/php/register_user.php?name=Ahmad%20Hanis&email=slumberjer@gmail.com&phone=01949494959&password=123456
+
+function sendEmail($useremail) {
+    $to      = $useremail; 
+    $subject = 'Verification for My.Grocery'; 
+    $message = 'http://slumberjer.com/grocery/php/verify.php?email='.$useremail; 
+    $headers = 'From: noreply@mygrocery.com' . "\r\n" . 
+    'Reply-To: '.$useremail . "\r\n" . 
+    'X-Mailer: PHP/' . phpversion(); 
+    mail($to, $subject, $message, $headers); 
+}
+
 ?>
