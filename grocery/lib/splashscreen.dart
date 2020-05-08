71c48e4bacdda70fb09a83b1f97e25efb4b6f102
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2020-05-08 08:57:07 +0800

    Update splashscreen.dart

--- grocery/lib/splashscreen.dart
+++ grocery/lib/splashscreen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:grocery/loginscreen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'mainscreen.dart';
+import 'user.dart';
+import 'package:toast/toast.dart';
 
 void main() => runApp(MyApp());
 
@@ -57,6 +61,7 @@ class _ProgressIndicatorState extends State<ProgressIndicator>
   @override
   void initState() {
     super.initState();
+    
     controller = AnimationController(
         duration: const Duration(milliseconds: 2000), vsync: this);
     animation = Tween(begin: 0.0, end: 1.0).animate(controller)
@@ -64,10 +69,13 @@ class _ProgressIndicatorState extends State<ProgressIndicator>
         setState(() {
           //updating states
           if (animation.value > 0.99) {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) => LoginScreen()));
+            controller.stop();
+            loadpref(this.context);
+            
+            // Navigator.push(
+            //     context,
+            //     MaterialPageRoute(
+            //         builder: (BuildContext context) => LoginScreen()));
           }
         });
       });
@@ -91,5 +99,58 @@ class _ProgressIndicatorState extends State<ProgressIndicator>
         valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
       ),
     ));
+  }
+
+  void loadpref(BuildContext ctx) async {
+    print('Inside loadpref()');
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String email = (prefs.getString('email') ?? '');
+    String pass = (prefs.getString('pass') ?? '');
+    print("Splash:Preference" + email + "/" + pass);
+    if (email.length > 5) {
+      //login with email and password
+      loginUser(email, pass, ctx);
+    } else {
+      loginUser("unregistered","123456789",ctx);
+    }
+  }
+
+  void loginUser(String email, String pass, BuildContext ctx) {
+   
+    http.post("https://slumberjer.com/grocery/php/login_user.php", body: {
+      "email": email,
+      "password": pass,
+    })
+        //.timeout(const Duration(seconds: 4))
+        .then((res) {
+      print(res.body);
+      var string = res.body;
+      List userdata = string.split(",");
+      if (userdata[0] == "success") {
+        User _user = new User(
+            name: userdata[1],
+            email: email,
+            password: pass,
+            phone: userdata[3],
+            credit: userdata[4],
+            datereg: userdata[5],
+            quantity: userdata[6]);
+   
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => MainScreen(
+                      user: _user,
+                    )));
+      } else {
+        Toast.show("Fail to login with stored credential. Login as unregistered account.", context,
+          duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+        loginUser("unregistered","123456789",ctx);
+       }
+    }).catchError((err) {
+      print(err);
+   
+    });
   }
 }
