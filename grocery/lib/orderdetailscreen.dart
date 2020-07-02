be476478027f8b77ea0b4c5d73af29c68297b9f4
Ahmad Hanis <36687333+ahmadhanis@users.noreply.github.com>
2020-07-02 16:14:25 +0800

    Add files via upload

--- grocery/lib/orderdetailscreen.dart
+++ grocery/lib/orderdetailscreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'order.dart';
 import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
 
 class OrderDetailScreen extends StatefulWidget {
   final Order order;
@@ -17,7 +18,7 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
   List _orderdetails;
   String titlecenter = "Loading order details...";
   double screenHeight, screenWidth;
-
+  String server = "https://slumberjer.com/grocery";
   @override
   void initState() {
     super.initState();
@@ -61,54 +62,62 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
                             child: InkWell(
                                 onTap: null,
                                 child: Card(
-                                  elevation: 10,
-                                  child: Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: <Widget>[
-                                      Expanded(
-                                          flex: 1,
-                                          child: Text(
-                                            (index + 1).toString(),
-                                            style:
-                                                TextStyle(color: Colors.white),
-                                          )),
-                                      Expanded(
-                                          flex: 2,
-                                          child: Text(
-                                            _orderdetails[index]['id'],
-                                            style:
-                                                TextStyle(color: Colors.white),
-                                          )),
-                                      Expanded(
-                                          flex: 4,
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: <Widget>[
-                                              Text(
-                                                _orderdetails[index]['name'],
+                                    elevation: 10,
+                                    child: Padding(
+                                      padding: EdgeInsets.all(5),
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: <Widget>[
+                                          Expanded(
+                                              flex: 1,
+                                              child: Text(
+                                                (index + 1).toString(),
                                                 style: TextStyle(
                                                     color: Colors.white),
-                                              ),
-                                              Text(
-                                                _orderdetails[index]
-                                                    ['cquantity'],
-                                                style: TextStyle(
-                                                    color: Colors.white),
-                                              ),
-                                            ],
-                                          )),
-                                      Expanded(
-                                        child: Text(
-                                          _orderdetails[index]['price'],
-                                          style: TextStyle(color: Colors.white),
-                                        ),
-                                        flex: 3,
+                                              )),
+                                          Expanded(
+                                            flex: 2,
+                                            child: Padding(
+                                                padding: EdgeInsets.all(3),
+                                                child: CachedNetworkImage(
+                                                  fit: BoxFit.fill,
+                                                  imageUrl: server +
+                                                      "/productimage/${_orderdetails[index]['id']}.jpg",
+                                                  placeholder: (context, url) =>
+                                                      new CircularProgressIndicator(),
+                                                  errorWidget:
+                                                      (context, url, error) =>
+                                                          new Icon(Icons.error),
+                                                )),
+                                          ),
+                                          Expanded(
+                                            flex: 4,
+                                            child: Text(
+                                              _orderdetails[index]['name'],
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                          Expanded(
+                                            flex: 1,
+                                            child: Text(
+                                              _orderdetails[index]['cquantity'],
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Text("RM "+
+                                              _orderdetails[index]['price'],
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                            flex: 2,
+                                          ),
+                                        ],
                                       ),
-                                    ],
-                                  ),
-                                )));
+                                    ))));
                       }))
         ]),
       ),
